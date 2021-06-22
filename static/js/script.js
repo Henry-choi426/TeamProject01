c199f23e8df5933498dec66b68be8352e71e9d6c
Henry-choi426 <84162474+Henry-choi426@users.noreply.github.com>
2021-06-22 23:00:13 +0900

    css

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,16 @@
 function authorize() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "")
-            alert("로그인 상태가 아닙니다. 로그인을 다시 해주세요.")
+      if (this.readyState == 4 && this.status == 200) {
+        if (this.responseText == "") {
+          console.log(this.responseText)
+          alert("로그인 상태가 아닙니다. 로그인을 다시 해주세요.")
+          location.href = "signinpage"
         };
-    }
+      };
+    };
     token = localStorage.getItem("access_token");
     xhttp.open("POST", "user_only");
-    xhttp.setRequestHeader("Authorization", "Bearer "+token);
+    xhttp.setRequestHeader("Authorization", "Bearer " + token);
     xhttp.send();
-}
+  }
